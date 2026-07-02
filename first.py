c58c67621568c0e1cc747e05334cd5d7bfbb52ba
Rishabh Nirmal <nirmalkartik42@gmail.com>
2026-07-02 22:08:41 +0530

    first commit

---
+++ first.py
@@ -0,0 +1,37 @@
+# variables 
+# name = "alex"
+# age = 30
+
+# print("My name is: ", name)
+# print("My age is: ", age)
+
+# print(type(name))
+# print(type(age))
+
+# sum 
+# a = 10
+# b = 20
+# c = a + b
+# print("Sum of a and b is:", c)
+
+
+# sub
+# a = 20
+# b = 10  
+# c = a - b
+# print("Subtraction of a and b is:", c)
+
+# multiply
+# a = 10
+# b = 20  
+# print("Multiplication of a and b is:", a * b)
+
+# # divide
+# a = 20
+# b = 10
+# print("Division of a and b is:", a / b)
+
+# Expression execution
+a,b = 10, 5
+txt = "@"
+print(a*txt*b)
