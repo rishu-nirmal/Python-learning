2ef9d02ae3425e408af5f9dc5d0de3384dfcfe60
Rishabh Nirmal <nirmalkartik42@gmail.com>
2026-07-04 20:15:18 +0530

    started with strings and its functions

--- first.py
+++ first.py
@@ -32,6 +32,101 @@
 # print("Division of a and b is:", a / b)
 
 # Expression execution
-a,b = 10, 5
-txt = "@"
-print(a*txt*b)
+# a,b = 10, 5
+# txt = "@"
+# print(a*txt*b)
+# import pyjokes
+
+# print("joke")
+# joke = pyjokes.get_joke()
+# print(joke)
+
+# Operators
+
+# arithmetic operators
+
+# a = 10
+
+# b = 20
+# b += 5
+# print(a + b)
+
+# comparison operators
+
+# a = 10<= 5
+# print(a)
+
+# r = 10 == 10
+# print(r)
+
+# logical operators
+
+# Truth table for OR operator
+
+# print("Result of True or False is:", True or False)
+# print("Result of True or True is:", True or True)
+# print("Result of False or True is:", False or True)
+# print("Result of False or False is:", False or False)
+
+# Truth table for AND operator
+
+# print("Result of True and False is:", True and False)
+# print("Result of True and True is:", True and True)
+# print("Result of False and True is:", False and True)
+# print("Result of False and False is:", False and False)
+
+# Truth table for not operator
+
+# print("Result of not True is:", not True)
+# print("Result of not False is:", not False)
+
+# Type conversion
+
+# a = 10
+# b = str(a)
+# print("Data type of a is:", type(b))
+
+# Input from user
+
+# a = input("Enter your name: ")
+# print("My name is:", a)
+
+# a = int(input("Enter your number 1: "))
+# b = int(input("Enter your number 2: "))
+# sum = a + b
+# print("Sum of a and b is:", sum)
+
+# Strings
+
+# name = "Alex"
+
+# short_name = name[0:3]
+# character1 = name[3]
+
+# print("Short name is:", short_name)
+# print("First character of name is:", character1)
+
+# a = "cristiano"
+# a[0:4:2]
+# print(a[0:2])
+
+# a = "cristiano"
+# a[0:]
+# print(a[0:])
+# print(a[:7])
+
+# a = "Pneumonoultramicroscopicsilicovolcanoconiosis" # To find the length of the string
+# print(len(a))
+
+# a = "Hello, World!" # to check if the string starts with "Hello"
+# print(a.endswith("ld!"))
+
+# a = "harish"
+# print(a.capitalize())
+
+# a = "I am in class 12 and I am a beautiful person" # to check if the string contains "beautiful"
+# print(a.replace("12", "13")) # to replace a word in the string
+# print(a.replace("beautiful", "wonderful"))
+
+# a = "hello everyone this is a great feeling\nwriting codes \\with \"python\""
+# print(a)
